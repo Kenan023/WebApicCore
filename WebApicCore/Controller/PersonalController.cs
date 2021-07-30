eea9bcbe32149e17bf143a4ad077f6996c6b3b40
Kenan023 <kenanhesenov2322@mail.ru>
2021-07-30 14:22:32 +0400

    First commit

---
+++ WebApicCore/Controller/PersonalController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApicCore.Model;
+
+namespace WebApicCore.Controller
+{
+    [ApiController]
+    public class PersonalController : ControllerBase
+    {
+        [Route("api/[controller]")]
+        [HttpGet]
+        public List<Personal> Personals()
+        {
+            return PersonalList.pers;
+        }
+        [Route("api/[controller]/{id}")]
+        [HttpGet]
+        public Personal Personals(int id)
+        {
+            Personal ischi = PersonalList.pers.Where(o => o.id == id).FirstOrDefault();
+            return ischi;
+        }
+    }
+}
---
+++ WebApicCore/PersonalList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApicCore.Model;
+
+namespace WebApicCore
+{
+    public class PersonalList
+    {
+        public static List<Personal> pers = new List<Personal>
+        {
+            new Personal
+            {
+                id=1000,
+                name="Kanan",
+                surname="Hasanov",
+                birth_place="Goycay",
+                age=20,
+                salary=1000
+
+            },
+             new Personal
+            {
+                id=1001,
+                name="Kamil",
+                surname="Xalilov",
+                birth_place="Baku",
+                age=25,
+                salary=900
+
+            },
+              new Personal
+            {
+                id=1002,
+                name="Ferid",
+                surname="Quliyev",
+                birth_place="Semkir",
+                age=19,
+                salary=1200
+            }
+        };
+    }
+}
